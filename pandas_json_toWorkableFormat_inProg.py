645db9a4e245e2794da1e67ee5dcaab9721bf298
Raymond <rejeuce07@gmail.com>
2015-12-01 18:33:18 -0800

    got rid of obsolete scripts

--- pandas_json_toWorkableFormat_inProg.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-train = pandas.read_json('trainSnip.json')
-id = train['id']
-cuisine = train['cuisine']
-ingredients = train['ingredients']
-ingArr = []
-for x in ingredients:
-	ingArr.append(ingredients[x])
